5a2ad5b17b71e703743b7a2d379fc10e4dbb6a80
AlbinaArabbaevna <albina_mirzaeva@mail.ru>
2015-09-15 11:39:52 +0300

    'JAVA'

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Альбина on 15.09.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, AlbinaArabbaevna!");
+    }
+}
